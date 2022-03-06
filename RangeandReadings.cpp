963dd16744d0d2d46307b8051f628199bca1858e
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-03-06 07:55:39 +0530

    Lets start step 2 --> code

---
+++ RangeandReadings.cpp
@@ -0,0 +1,11 @@
+#include "RangeandReadings.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string getRangeandReadingsinChargingSession(const int* chargingSession)
+{
+	string output = "4-5, 2";
+	return output;
+}
