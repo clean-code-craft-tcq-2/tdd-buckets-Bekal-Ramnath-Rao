30f1c017429a79898e8b1282ec68355f111c7e67
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-03-08 21:52:31 +0530

    Update test-tdd.cpp

--- test-tdd.cpp
+++ test-tdd.cpp
@@ -8,6 +8,6 @@ TEST_CASE("Check range and readings from charging session") {
   int chargingSession[] = {4,5};
   int size_of_charginSession = sizeof(chargingSession)/sizeof(chargingSession[0]);
   REQUIRE(getRangeandReadingsinChargingSession(chargingSession,size_of_charginSession) == "4-5, 2");
-  chargingSession[] = {1,1,1,2,2,3,3,3,3,4,4,4,4};
+  chargingSession[12] = {1,1,1,2,2,3,3,3,3,4,4,4};
   REQUIRE(getRangeandReadingsinChargingSession(chargingSession,size_of_charginSession) == "1-2, 5\n3-4, 8");
 }
