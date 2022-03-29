232df0c4ca29e9a6a63ca6521901ac07a15a96c1
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-03-29 21:02:28 +0530

    Add files via upload

--- RangeandReadings.h
+++ RangeandReadings.h
@@ -5,3 +5,5 @@ using namespace std;
 
 string getRangeandReadingsinChargingSession(int*, int, int);
 int convertDigitalToAnalog(const int*,int,int,void (*)(),int (*)(const int*,int));
+void printonConsole();
+int convertBinarytoDecimal(const int* ,int)
