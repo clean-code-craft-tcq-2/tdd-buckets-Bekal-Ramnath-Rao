4eb0571a8b6b3c3bae6ffdf18a9b06c5c103dc4f
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-03-29 21:03:55 +0530

    Update RangeandReadings.h

--- RangeandReadings.h
+++ RangeandReadings.h
@@ -6,4 +6,4 @@ using namespace std;
 string getRangeandReadingsinChargingSession(int*, int, int);
 int convertDigitalToAnalog(const int*,int,int,void (*)(),int (*)(const int*,int));
 void printonConsole();
-int convertBinarytoDecimal(const int* ,int)
+int convertBinarytoDecimal(const int* ,int);
