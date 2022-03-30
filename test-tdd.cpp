be58ce4e7ddc7d99750a4052308c633ebc3fdf3d
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-03-30 08:12:55 +0530

    Update test-tdd.cpp

--- test-tdd.cpp
+++ test-tdd.cpp
@@ -34,6 +34,6 @@ TEST_CASE("Check conversion Analog to digital for sensor that calculates chargin
   int size_of_Array = sizeof(Digital_value1)/sizeof(Digital_value1[0]);
   void (*funp_printOnConsole)() = printonConsole;
   int (*funp_convertBinarytoDecimal)(const int*,int) = convertBinarytoDecimal;
-  float (*funp_convertDecimaltoAnalog)(int,int,int) = convertDecimaltoAnalog;
+  float (*funp_convertDecimaltoAnalog)(int,int,int) = convertDecimaltoAnalog_chargingDischarging;
   REQUIRE(convertDigitalToAnalog(Digital_value1, size_of_Array, 15,10,funp_printOnConsole,funp_convertBinarytoDecimal,funp_convertDecimaltoAnalog) ==14);
 }
